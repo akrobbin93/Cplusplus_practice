493b9edf4d6bd3d072a93ba2c9a294aceeb5c631
akrobbin93 <akylerobbins@gmail.com>
2023-01-31 11:23:31 -0500

    LinkedIn Learning C++ Chapter 1 code along

---
+++ LI_ch01/GetName.cpp
@@ -0,0 +1,15 @@
+//Get Name from Console
+
+#include <iostream>
+#include <string>
+
+//using namespace std;
+
+int main()
+{
+    std::string str;
+    std::cout << "Please Enter Name:" << std::endl;
+    std::cin >> str;
+    std::cout << "Hello " << str << std::endl;
+    return(0);
+}
